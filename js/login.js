2ea6d5cb48529125bae3b14d9f09c9a18cef8055
Radhamante <58041359+Radhamante@users.noreply.github.com>
2019-12-10 12:48:39 +0100

    v2

--- js/login.js
+++ js/login.js
@@ -22,7 +22,30 @@ $(document).ready(function(){
         })
     })
 
-
+    $(document).keypress(function(event){
+	
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $.ajax({
+                url: api_url + "login/" + user +"/" + pass,
+                dataType: 'jsonp',
+                type: 'GET',
+                contentType: 'application/json; charset=utf-8',
+                success: function(result, status, xhr) {
+                    console.log(result);
+                    localStorage.setItem('token', result.result.token);
+                    localStorage.setItem('id', result.result.id);
+                    localStorage.setItem('name', user);
+                    window.location = "file:///C:/Users/Raphael%20D/Desktop/TRAVAIL/Kwick/main.html"
+                },
+                error: function(xhr, status, error) {
+                    $(signup_username).css("border", "#ec4e20 1px solid")
+                    $(signup_password).css("border", "#ec4e20 1px solid")
+                }
+            })
+        }
+    
+    });
 
 });
 
--- js/main.js
+++ js/main.js
@@ -4,52 +4,71 @@ $(document).ready(function(){
     // }
 
     ////////////////////////       AFFICHAGE DES USERS         ///////////////////////////////
-    
-    $.ajax({
-        url: "http://greenvelvet.alwaysdata.net/kwick/api/user/logged/" + localStorage.getItem('token'),
-        dataType: 'jsonp',
-        type: 'GET',
-        contentType: 'application/json; charset=utf-8',
-        success: function(result, status, xhr) {
-            for (let index = 0; index < result.result.user.length; index++) {
-                $("#user").append( "<div><i class='far fa-dot-circle'></i><p class='all_user'>" + result.result.user[index] + "</b></div>")
-            }  
-        },
-        error: function(xhr, status, error) {
-        }
-    })
+    setInterval(function(){
+        $.ajax({
+            url: "http://greenvelvet.alwaysdata.net/kwick/api/user/logged/" + localStorage.getItem('token'),
+            dataType: 'jsonp',
+            type: 'GET',
+            contentType: 'application/json; charset=utf-8',
+            success: function(result, status, xhr) {
+                
+                $( ".userrs" ).remove();
+                for (let index = 0; index < result.result.user.length; index++) {
+                    $("#user").append( "<div class='userrs'><i class='far fa-dot-circle'></i><p class='all_user'>" + result.result.user[index] + "</b></div>")
+                }  
+            },
+            error: function(xhr, status, error) {
+            }
+        })
+    },1000)
 
     /////////////////////////     affichage des messages ////////////////////////
-
-    $.ajax({
-        url: "http://greenvelvet.alwaysdata.net/kwick/api/talk/list/" + localStorage.getItem('token') + "/0",
-        dataType: 'jsonp',
-        type: 'GET',
-        contentType: 'application/json; charset=utf-8',
-        success: function(result, status, xhr) {
-            console.log(result)
-            for (let index = 0; index < result.result.talk.length; index++) {
-                if (localStorage.getItem("name") == result.result.talk[index].user_name.toLowerCase()) {
-                    $("#messageList").append("<div class='msg personnal'><span>" + result.result.talk[index].user_name + "</span><p class='test'>" + result.result.talk[index].content + "</p></div>")
-                    
-                }else{
-                    $("#messageList").append("<div class='msg'><span>" + result.result.talk[index].user_name + "</span><p>" + result.result.talk[index].content + "</p></div>")
+    sessionStorage.setItem("time",0)
+    function showmessage() {
+        $.ajax({
+            
+            url: "http://greenvelvet.alwaysdata.net/kwick/api/talk/list/" + localStorage.getItem('token') + "/" + sessionStorage.getItem('time'),
+            dataType: 'jsonp',
+            type: 'GET',
+            contentType: 'application/json; charset=utf-8',
+            success: function(result, status, xhr) {
+                for (let index = 0; index < result.result.talk.length; index++) {
+                    console.log(result)
+                    if (localStorage.getItem("name") == result.result.talk[index].user_name.toLowerCase()) {
+                        $("#messageList").append("<div class='msg personnal'><span>" + result.result.talk[index].user_name + "</span><p>" + result.result.talk[index].content + "</p></div>");
+                        
+                        
+                    }else{
+                        $("#messageList").append("<div class='msg'><span>" + result.result.talk[index].user_name + "</span><p>" + result.result.talk[index].content + "</p></div>");
+                    }
+                    sessionStorage.setItem("time",result.result.last_timestamp);
+                    scrollBot();
                 }
+                
+                
+            },
+            error: function(xhr, status, error) {
             }
-            $('#messageList').scrollTop($('#messageList')[0].scrollHeight);
-        },
-        error: function(xhr, status, error) {
-        }
-    })
+        })
+    }
+    function scrollBot(){
+        $('#messageList').scrollTop($('#messageList')[0].scrollHeight);
+    }
+    
+    setInterval(function(){showmessage()},1000)
+    // $(".test").click(function(){
+    //     console.log("chibre")
+    //     $(".test").append("<span>" + "test" + "</span>")
+    // });
 
-    $(".test").click(function(){
-        console.log("chibre")
-        $(".test").append("<span>" + "test" + "</span>")
-    });
 
-      ///////////////////////////    envoie d'un message     /////////////////////////
 
+    ///////////////////////////    envoie d'un message     /////////////////////////
+   
     $("#send_message").on("click", function(){
+        send_message()
+    })
+    function send_message() {
         $.ajax({
             url: "http://greenvelvet.alwaysdata.net/kwick/api/say/" + localStorage.getItem('token') + "/" + localStorage.getItem('id') + "/" + $("#message_content").val(),
             dataType: 'jsonp',
@@ -58,12 +77,18 @@ $(document).ready(function(){
             success: function(result, status, xhr) {
                 $("#message_content").val('')
                 $("#message_content").css("border", "#c0d6df 1px solid")
+                showmessage()
+                scrollBot()
+                
             },
             error: function(xhr, status, error) {
                 $("#message_content").css("border", "#ec4e20 1px solid")
             }
         })
-    })
+    }
+        
+    
+        
 
     /////////////////////////////   disconnect     //////////////
     $("#disconnect").on("click",function(){
@@ -89,5 +114,14 @@ $(document).ready(function(){
         $("#disconnect_menu_background").css("display","none");
         $("#disconnect_menu").css("display","none");
     });
+
+    // $('#message_content').keypress(function(event){
+	
+    //     var keycode = (event.keyCode ? event.keyCode : event.which);
+    //     if(keycode == '13'){
+    //         send_message()	
+    //     }
     
+    // });
+
 });
